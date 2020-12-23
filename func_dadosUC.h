50e6344e8c96089c6cbc2c7e0cf2f593e68ecc3a
Sgrzz <alexandrefr_30@hotmail.com>
2020-12-23 02:16:47 +0000

    Revert "novas func dadosUC"
    This reverts commit 42f56d25ad27d62400f54618de3a7e944812ea53.

--- func_dadosUC.h
+++ func_dadosUC.h
@@ -2,22 +2,10 @@
 #define FUNC_DADOSUC_H_INCLUDED
 
 
-//func ler dados para a struct dadosUC
 int lerDadosCodigoUC();
-void lerDadosNomeUC(char nome[MAX_STRING_NOME_AULA]);
-int lerTipoUC();
-int lerSemestreUC();
-int lerRegimeUC();
-int lerQuantTipoAulas_T();
-int lerQuantTipoAulas_TP();
-int lerQuantTipoAulas_PL();
-int lerDuracaoAulas_T();
-int lerDuracaoAulas_TP();
-int lerDuracaoAulas_PL();
-//func que chama cada uma das func atraz
+
 dadosUC inserirDadosUC();
-//func que altera o valor dos dados especificado
-void alterarDadosUC(dadosUC *dadosDaUC, int opcao);
+void alterarDadosUC(dadosUC *dadosDaUC, int option);
 
 
 #endif // FUNC_DADOSUC_H_INCLUDED
